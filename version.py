b749ad8b04ecd0710a34520f05418c144d534dc7
HTMLGuy <shane@htmlguy.com>
2026-02-21 11:04:41 -0500

    chore: revert version to 0.6.0 for release workflow

--- version.py
+++ version.py
@@ -3,7 +3,7 @@ Dispatcharr version information.
 """
 #!/usr/bin/env python3
 
-__version__ = "0.7.0"
+__version__ = "0.6.0"
 __version_tag__ = "HLS Output Proxy"
 __version_date__ = "2026-02-21"
 __timestamp__ = None    # Set during CI/CD build process
